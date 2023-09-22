aa83665de96139ddefa62655b16e636b4cd26065
MichaelHofbeck <hofbeckm@bc.edu>
2023-09-22 11:25:47 -0400

    updates

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,8 @@
 import { Html } from "@react-three/drei"
 import Client from "./api/Client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { OrbitControls } from "@react-three/drei"
+import { MeshNormalMaterial } from "three"
 
 
 export default function Experience()
@@ -9,33 +10,56 @@ export default function Experience()
 
     const [espnS2, setEspnS2] = useState('')
     const [SWID, setSWID] = useState('')
+    const client = new Client()
     // const client = new Client({ leagueId: 1319421077 })
     // console.log(client.getCurrentStandings({ seasonId: 2023, scoringPeriodId: 2 }))
     const setCookies = () =>
     {
         localStorage.setItem('espnS2', espnS2)
         localStorage.setItem('SWID', SWID)
+        
     }
+
+    const getCurrentStandings = async () =>
+    {
+        client.setOptions({ leagueId: 1319421077, espnS2: localStorage.getItem('espnS2'), SWID: localStorage.getItem('SWID') })
+        console.log(client)
+        const standings = await client.getCurrentStandings({ seasonId: 2023, scoringPeriodId: 2 })
+        console.log(standings)
+    }
+
+    // useEffect(() =>
+    // {
+    //     if (localStorage.getItem('espnS2') !== '' && localStorage.getItem('SWID') !== '')
+    //     {
+    //         client.setOptions({ leagueId: 1319421077, espnS2: localStorage.getItem('espnS2'), SWID: localStorage.getItem('SWID') })
+    //     }
+    // }, [espnS2, SWID])
 
     return <>
     <OrbitControls />
 
     <mesh>
-        <boxGeometry />
-        <meshBasicMaterial />
+        <boxGeometry args={ [ 1, 1, 1 ] }/>
+        <meshNormalMaterial />
     </mesh>
 
-    <Html position={[ 1, 0, 0]}>
+    <Html position={[ 1, 1, 1]} occlude="blending">
         <form>
             <label>
                 espnS2
-                <input type="text" value={espnS2} onChange={(event) => setEspnS2(event.target.value)} />
+                <input type="text" value={espnS2} onChange={(event) => 
+                {
+                    
+                    setEspnS2(event.target.value)
+                }} />
             </label>
             <label>
                 SWID
                 <input type="text" value={SWID} onChange={(event) => setSWID(event.target.value)} />
             </label>
-            <input type="submit" value="Submit" onClick={setCookies} />
+            <input type="submit" value="Submit" onClick={(e) => {e.preventDefault(); setCookies();}} />
+            <input type="submit" value="Submit" onClick={(e) => {e.preventDefault(); getCurrentStandings();}} />
         </form>
     </Html>
     </>
--- src/api/Client.js
+++ src/api/Client.js
@@ -6,19 +6,25 @@ class Client
     constructor(options = {})
     {
         this.leagueId = options.leagueId
-        this.header = { Cookie: `espn_s2=${this.espnS2}; SWID=${this.SWID};` }
+    }
+
+    setOptions({ leagueId, espnS2, SWID })
+    {
+        this.leagueId = leagueId
+        this.espnS2 = espnS2
+        this.SWID = SWID
     }
 
     getCurrentStandings({ seasonId, scoringPeriodId })
     {
         const route = `https://fantasy.espn.com/apis/v3/games/ffl/seasons/${seasonId}/segments/0/leagues/${this.leagueId}?scoringPeriodId=${scoringPeriodId}&view=mRoster&view=mTeam`
         const headers = { Cookie: `espn_s2=${this.espnS2}; SWID=${this.SWID};`, }
-        axios.get(route, {headers, withCredentials: true } ).then((response) => 
+        const standings = axios.get(route,{headers: headers, withCredentials: true } ).then((response) => 
         {
-            console.log(response)
             const data = _.get(response.data, 'teams')
             return data
         })
+        return standings
     }
 }
 
